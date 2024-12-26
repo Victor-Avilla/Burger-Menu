d98ffbe4d9c9b6b97c7dece867b84ee272b27d31
Victor-Avilla <victorfonsecaavila@gmail.com>
2024-12-26 10:47:59 -0300

    reduce+filter changes

--- scripts.js
+++ scripts.js
@@ -1,17 +1,44 @@
 const list = document.querySelector('ul')
-let myLi = ''
 
-showAll = () => {
-    menuOptions.forEach((product) => {
+
+const formatCurrency = (value) => {
+    return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+}
+showAll = (productsArray) => {
+    let myLi = ''
+
+    productsArray.forEach((products) => {
         myLi +=
             `
         <li>   
-                <p class="name">${product.name}</p>
-                <img src=${product.src}>
-                <p class="price">${'Price: U$' + product.price + ',00'}</p> 
+                <p class="name">${products.name}</p>
+                <img src=${products.src}>
+                <p class="price">${'Price: ' + formatCurrency(products.price)}</p> 
         </li>
     `
     })
     list.innerHTML = myLi
 }
 
+apllyDiscount = () => {
+    const discount = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.5
+    }))
+    showAll(discount)
+}
+
+sumPrices = () => {
+    const sum = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML =
+        `
+         <li> 
+                <p class="price">${'Price: ' + formatCurrency(sum)}</p> 
+        </li>
+        `
+}
+veganFilter = () => {
+    const filt = menuOptions.filter((product) => product.vegan === true)
+    showAll(filt)
+}
